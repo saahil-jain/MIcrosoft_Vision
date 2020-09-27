e39c9d265a18662de8908eb4b43f722ab7bb71c0
saahil-jain <saahiljain98@gmail.com>
2020-09-27 18:29:44 +0530

    training data

---
+++ face_mask_detection/training/train.py
@@ -0,0 +1,113 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import pickle
+import cv2
+import os
+
+INIT_LR = 1e-4
+EPOCHS = 10
+BS = 32
+image_dimension = 200
+
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images("dataset"))
+data = []
+labels = []
+for imagePath in imagePaths:
+	label = imagePath.split(os.path.sep)[-2]
+
+	image = load_img(imagePath, target_size=(image_dimension, image_dimension))
+	image = img_to_array(image)
+	image = preprocess_input(image)
+
+	data.append(image)
+	labels.append(label)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+# print(lb.inverse_transform(np.array([0])))
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+aug = ImageDataGenerator(
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+baseModel = MobileNetV2(weights="imagenet", include_top=False, input_shape=(image_dimension, image_dimension, 3))
+for layer in baseModel.layers:
+	layer.trainable = False
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+maskNet = Model(inputs=baseModel.input, outputs=headModel)
+
+print("[INFO] compiling model...")
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+maskNet.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+print("[INFO] training head...")
+H = maskNet.fit(
+	aug.flow(trainX, trainY, batch_size=BS),
+	steps_per_epoch=len(trainX) // BS,
+	validation_data=(testX, testY),
+	validation_steps=len(testX) // BS,
+	epochs=EPOCHS)
+
+print("[INFO] evaluating network...")
+predIdxs = maskNet.predict(testX, batch_size=BS)
+predIdxs = np.argmax(predIdxs, axis=1)
+print(classification_report(testY.argmax(axis=1), predIdxs,
+	target_names=lb.classes_))
+
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+
+print("[INFO] saving maskNet model...")
+os.makedirs('./outputs/model', exist_ok = True)
+maskNet_path = "./outputs/model/maskNet_model.h5"
+maskNet.save(maskNet_path)
+
+print("[INFO] saving graph...")
+os.makedirs("./images", exist_ok = True)
+plot_path = "./images/plot.png"
+plt.savefig(plot_path)
